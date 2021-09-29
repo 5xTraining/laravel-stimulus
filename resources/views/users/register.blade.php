b674c7c2a8933ece4be071216f0a0498c7521c9c
eddie <eddie@5xruby.tw>
2021-09-29 16:40:03 +0800

    add modal and styles

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts/application')
 
 @section('body')
-<h1 class="text-6xl font-thin mb-2">會員註冊</h1>
+<h1 class="text-6xl font-thin mb-4">會員註冊</h1>
+
+<form action="/users" method="post">
+    <input type="text" class="demo mr-2">
+    <button class="demo">註冊</button>
+
+    <!-- modal -->
+    <div class="modal-form">
+        <div class="modal-form-dialog">
+            <div class="px-6 py-8 h-full">
+                <header class="modal-form-header">
+                    <h3 class="text-4xl font-thin">哈囉？</h3>
+                    <button class="close-btn">X</button>
+                </header>
+                <div>
+                    <p class="text-lg"> 請填寫會員資訊，不然...我好像也不能拿你怎樣！ </p>
+                </div>
+            </div>
+            <footer class="modal-form-footer">
+                <button class="close-text-btn">我知道了</button>
+            </footer>
+        </div>
+    </div>
+    <!-- modal -->
+</form>
 
 @endsection
